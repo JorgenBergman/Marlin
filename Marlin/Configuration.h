c761de7a1c75f05d89b4777429cdd9735e86b657
JorgenBergman <114925994+JorgenBergman@users.noreply.github.com>
2022-10-31 10:13:54 +0100

    update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2215,7 +2215,7 @@
 // Preheat Constants - Up to 10 are supported without changes
 //
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 208
+#define PREHEAT_1_TEMP_HOTEND 210
 #define PREHEAT_1_TEMP_BED     60
 //#define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
